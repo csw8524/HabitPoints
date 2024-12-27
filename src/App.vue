9d4d03db595444d8d6ade08bd4a8c202402fdcad
csw8524 <swchou524@gmail.com>
2024-12-27 11:06:32 +0800

    feat: 側邊欄

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,5 @@
 <template>
-  <RouterView />
-  <div>
-    <!-- 手機版底部選單 -->
-    <div class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md md:hidden">
-      <div class="flex justify-around items-center py-2">
-        <RouterLink to="/" class="flex flex-col items-center text-gray-600 hover:text-blue-500">
-          <i class="fas fa-home text-xl"></i>
-          <span class="text-xs">首頁</span>
-        </RouterLink>
-        <RouterLink
-          to="/cards"
-          class="flex flex-col items-center text-gray-600 hover:text-blue-500"
-        >
-          <i class="fas fa-gift text-xl"></i>
-          <span class="text-xs">集點卡</span>
-        </RouterLink>
-        <RouterLink
-          to="/rewards"
-          class="flex flex-col items-center text-gray-600 hover:text-blue-500"
-        >
-          <i class="fas fa-trophy text-xl"></i>
-          <span class="text-xs">獎勵</span>
-        </RouterLink>
-        <RouterLink
-          to="/profile"
-          class="flex flex-col items-center text-gray-600 hover:text-blue-500"
-        >
-          <i class="fas fa-user text-xl"></i>
-          <span class="text-xs">個人資料</span>
-        </RouterLink>
-      </div>
-    </div>
-
+  <div class="w-full h-full pt-4 flex">
     <!-- 電腦版側邊欄選單 -->
     <div
       class="hidden md:flex flex-col fixed top-0 left-0
@@ -65,6 +33,41 @@
         >
           <i class="fas fa-user text-lg"></i>
           <span class="ml-3">個人資料</span>
+        </RouterLink>
+      </div>
+    </div>
+    <div class="flex-1 md:ml-64">
+      <RouterView />
+    </div>
+    <!-- 手機版底部選單 -->
+    <div
+      class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md md:hidden"
+    >
+      <div class="flex justify-around items-center py-2">
+        <RouterLink to="/" class="flex flex-col items-center text-gray-600 hover:text-blue-500">
+          <i class="fas fa-home text-xl"></i>
+          <span class="text-xs">首頁</span>
+        </RouterLink>
+        <RouterLink
+          to="/cards"
+          class="flex flex-col items-center text-gray-600 hover:text-blue-500"
+        >
+          <i class="fas fa-gift text-xl"></i>
+          <span class="text-xs">集點卡</span>
+        </RouterLink>
+        <RouterLink
+          to="/rewards"
+          class="flex flex-col items-center text-gray-600 hover:text-blue-500"
+        >
+          <i class="fas fa-trophy text-xl"></i>
+          <span class="text-xs">獎勵</span>
+        </RouterLink>
+        <RouterLink
+          to="/profile"
+          class="flex flex-col items-center text-gray-600 hover:text-blue-500"
+        >
+          <i class="fas fa-user text-xl"></i>
+          <span class="text-xs">個人資料</span>
         </RouterLink>
       </div>
     </div>
